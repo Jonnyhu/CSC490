1c0a1d3e4805acfff11ddbb4ff481d52f8a6daef
Jonnyhu <jonny.hu@mail.utoronto.ca>
2014-11-23 16:32:41 -0500

    IR update

--- IR/IR_send_receive/IR_send_receive.ino
+++ IR/IR_send_receive/IR_send_receive.ino
@@ -1,55 +1,154 @@
-//define pins. I used pins 4 and 5
-#define irLedPin 13          // IR Led on this pin
-#define irSensorPin 2       // IR sensor on this pin
-#define delaytime 1000
+#define IRpin_PIN      PIND
+#define IRpin          2
+#define IRledPin       13
 
-int irRead(int readPin, int triggerPin); //function prototype
-int array[5] = {1, 0, 1, 1, 0};
+// the brightness of the IR Red
+#define POWER 130
 
-void setup()
-{
-  pinMode(irSensorPin, INPUT);
-  pinMode(irLedPin, OUTPUT);
-  Serial.begin(9600); 
-  // prints title with ending line break 
-  Serial.println("Program Starting"); 
-  // wait for the long string to be sent 
-  delay(100); 
+// the maximum pulse we'll listen for - 65 milliseconds is a long time
+#define MAXPULSE 65000
+#define NUMPULSES 50
+
+// what our timing resolution should be, larger is better
+// as its more 'precise' - but too large and you wont get
+// accurate timing
+#define RESOLUTION 20 
+
+// What percent we will allow in variation to match the same code
+#define FUZZINESS 20
+
+// we will store up to 100 pulse pairs (this is -a lot-)
+uint16_t pulses[NUMPULSES][2];  // pair is high and low pulse 
+uint8_t currentpulse = 0; // index for pulses we're storing
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("Ready to decode IR!");
 }
 
-void loop()
-{  
-  //Serial.println(irRead(irSensorPin, irLedPin)); //display the results
-  for (int i=0; i < 5; i++){
-    if (array[i] != 0) {
-      irSend(irLedPin);
+void loop(void) {
+  SendNikonCode();
+  /*int numberpulses;
+  
+  numberpulses = listenForIR();
+  
+  Serial.print("Heard ");
+  Serial.print(numberpulses);
+  Serial.println("-pulse long IR signal");
+  delay(500);*/
+  
+}
+
+
+
+int listenForIR(void) {
+  currentpulse = 0;
+  
+  while (1) {
+    uint16_t highpulse, lowpulse;  // temporary storage timing
+    highpulse = lowpulse = 0; // start out with no pulse length
+  
+//  while (digitalRead(IRpin)) { // this is too slow!
+    while (IRpin_PIN & (1 << IRpin)) {
+       // pin is still HIGH
+
+       // count off another few microseconds
+       highpulse++;
+       delayMicroseconds(RESOLUTION);
+
+       // If the pulse is too long, we 'timed out' - either nothing
+       // was received or the code is finished, so print what
+       // we've grabbed so far, and then reset
+       
+       // KGO: Added check for end of receive buffer
+       if (((highpulse >= MAXPULSE) && (currentpulse != 0))|| currentpulse == NUMPULSES) {
+         return currentpulse;
+       }
     }
+    // we didn't time out so lets stash the reading
+    pulses[currentpulse][0] = highpulse;
+  
+    // same as above
+    while (! (IRpin_PIN & _BV(IRpin))) {
+       // pin is still LOW
+       lowpulse++;
+       delayMicroseconds(RESOLUTION);
+        // KGO: Added check for end of receive buffer
+        if (((lowpulse >= MAXPULSE)  && (currentpulse != 0))|| currentpulse == NUMPULSES) {
+         return currentpulse;
+       }
+    }
+    pulses[currentpulse][1] = lowpulse;
+
+    // we read one high-low pulse successfully, continue!
+    currentpulse++;
   }
-  //Serial.println(irRead(irSensorPin));
-  //delay(10); //wait for the string to be sent
 }
-//
-/******************************************************************************
- * This function can be used with a panasonic pna4602m ir sensor
- * it returns a zero if something is detected by the sensor, and a 1 otherwise
- * The function bit bangs a 38.5khZ waveform to an IR led connected to the
- * triggerPin for 1 millisecond, and then reads the IR sensor pin to see if
- * the reflected IR has been detected
- for (i=0; i <=cycles; i++)
-  {
-    digitalWrite(triggerPin, HIGH); 
-    delayMicroseconds(halfPeriod);
-    digitalWrite(triggerPin, LOW); 
-    delayMicroseconds(halfPeriod - 1);     // - 1 to make up for digitaWrite overhead    
+void printpulses(void) {
+  Serial.println("\n\r\n\rReceived: \n\rOFF \tON");
+  for (uint8_t i = 0; i < currentpulse; i++) {
+    Serial.print(pulses[i][0] * RESOLUTION, DEC);
+    Serial.print(" usec, ");
+    Serial.print(pulses[i][1] * RESOLUTION, DEC);
+    Serial.println(" usec");
   }
- ******************************************************************************/
-int irRead(int readPin)
-{
-  return digitalRead(readPin);
+  
+  // print it in a 'array' format
+  Serial.println("int IRsignal[] = {");
+  Serial.println("// ON, OFF (in 10's of microseconds)");
+  for (uint8_t i = 0; i < currentpulse-1; i++) {
+    Serial.print("\t"); // tab
+    Serial.print(pulses[i][1] * RESOLUTION / 10, DEC);
+    Serial.print(", ");
+    Serial.print(pulses[i+1][0] * RESOLUTION / 10, DEC);
+    Serial.println(",");
+  }
+  Serial.print("\t"); // tab
+  Serial.print(pulses[currentpulse-1][1] * RESOLUTION / 10, DEC);
+  Serial.print(", 0};");
 }
 
-void irSend(int triggerPin)
-{
-  digitalWrite(triggerPin, HIGH);
-  delay(1000);
+// This procedure sends a 38KHz pulse to the IRledPin 
+// for a certain # of microseconds. We'll use this whenever we need to send codes
+void pulseIR(long microsecs) {
+  // we'll count down from the number of microseconds we are told to wait
+ 
+  cli();  // this turns off any background interrupts
+ 
+  while (microsecs > 0) {
+    // 38 kHz is about 13 microseconds high and 13 microseconds low
+   digitalWrite(IRledPin, HIGH);  // this takes about 3 microseconds to happen
+   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+   digitalWrite(IRledPin, LOW);   // this also takes about 3 microseconds
+   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+ 
+   // so 26 microseconds altogether
+   microsecs -= 26;
+  }
+ 
+  sei();  // this turns them back on
+}
+
+void SendNikonCode() {
+  // This is the code for my particular Nikon, for others use the tutorial
+  // to 'grab' the proper code from the remote
+ 
+  pulseIR(2080);
+  delay(27);
+  pulseIR(440);
+  delayMicroseconds(1500);
+  pulseIR(460);
+  delayMicroseconds(3440);
+  pulseIR(480);
+ 
+ 
+  delay(65); // wait 65 milliseconds before sending it again
+ 
+  pulseIR(2000);
+  delay(27);
+  pulseIR(440);
+  delayMicroseconds(1500);
+  pulseIR(460);
+  delayMicroseconds(3440);
+  pulseIR(480);
 }
